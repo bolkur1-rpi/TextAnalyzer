ccd2c4509c47c2d678f05dc55a40fa89e91ad717
Slow4life <O-helmdal@hotmail.com>
2024-02-14 16:52:57 +0000

    View submission info

--- html/index.php
+++ html/index.php
@@ -12,8 +12,10 @@ if(!isset($_SESSION['login'])) { header("Location: /php/login.php"); }
 ?>
 
 <script>
-  function populateContent(fileName) {
-    document.getElementById("fileName").innerHTML = fileName;
+  function populateContent(fileName, words, uniqueWords) {
+    document.getElementById("displayName").innerHTML = fileName;
+    document.getElementById("displayName").innerHTML = words;
+    document.getElementById("displayName").innerHTML = uniqueWords;
   }
 </script>
 
@@ -40,7 +42,9 @@ if(!isset($_SESSION['login'])) { header("Location: /php/login.php"); }
 <body>
 <div id="wrapper">
   <div id="content">
-    <h2 id="fileName">asd</h2>
+    <a id="displayName">Display name</a>
+    <a id="wordCount">Word Count</a>
+    <a id="wordCountUnique">Uniqie word count</a>
   </div>
   <div id="submissions">
   <?php
@@ -66,14 +70,14 @@ if(!isset($_SESSION['login'])) { header("Location: /php/login.php"); }
     die("Connection failed: " . $conn->connect_error);
   }
 
-  $sql = "SELECT * FROM paper, student WHERE student.student_name = '".$_SESSION['login']."'";
+  $sql = "SELECT * FROM paper, student WHERE student.id = paper_id AND student.student_name = '".$_SESSION['login']."'";
   $result = $conn->query($sql);
   $_SESSION['sub_count'] = $result->num_rows;
   echo "<h2>Your submissions: <br></h2>";
   if ($result->num_rows > 0) {
       // Output submissions
       while($row = $result->fetch_assoc()) {
-        echo "<button class='borderButton'><img src='img/read.png' onclick='populateContent(\"$row[paper_name]\", \"$row[paper_name]\")' alt='Info' height='12' width='12' title='Info'></img></button>";
+        echo "<button class='borderButton' onclick='populateContent(\"$row[paper_display_name]\", \"$row[number_of_words]\", \"$row[number_of_unique_words]\")'><img src='img/read.png' alt='Info' height='12' width='12' title='Info'></img></button>";
         echo " " . $row["paper_name"] . "<br>";
       }
   } else {
