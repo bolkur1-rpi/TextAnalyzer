5febc9ebdc0da27d7239e1e8f8c0963e74d2862e
Jogvan Patursson <jogvan.pat18@student.setur.fo>
2024-02-14 15:38:05 +0000

    Changed database. student_name is now primary key, student_id is deleted. paper_name is now primary key, paper_id is deleted.

--- SQL/ddl.sql
+++ SQL/ddl.sql
@@ -4,21 +4,19 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS student;
 SET FOREIGN_KEY_CHECKS = 1;
 CREATE TABLE student(
-    student_id int NOT NULL AUTO_INCREMENT,
     student_name varchar(255) NOT NULL UNIQUE,
-    PRIMARY KEY (student_id)
+    PRIMARY KEY (student_name)
 );
 
 SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS paper;
 SET FOREIGN_KEY_CHECKS = 1;
 CREATE TABLE paper(
-    paper_id int NOT NULL AUTO_INCREMENT,
     paper_name varchar(255) NOT NULL UNIQUE,
     paper_display_name varchar(255) NOT NULL,
     number_of_words int NOT NULL,
     number_of_unique_words int NOT NULL,
     student_id int,
-    PRIMARY KEY (paper_id),
-    FOREIGN KEY(student_id) REFERENCES student(student_id)
+    PRIMARY KEY (paper_name),
+    FOREIGN KEY(student_name) REFERENCES student(student_name)
 );
--- SQL/dml.sql
+++ SQL/dml.sql
@@ -14,44 +14,55 @@ USE textdb;
 -- ------------------------------------
 --       RETURNS ALL STUDENT IDS
 -- ------------------------------------
-DROP PROCEDURE IF EXISTS getAllStudentId;
+DROP PROCEDURE IF EXISTS getAllStudentNames;
 DELIMITER $$
-CREATE PROCEDURE getAllStudentId()
+CREATE PROCEDURE getAllStudentNames()
 BEGIN
-SELECT student_id FROM student;
+SELECT student_name FROM student;
 END $$
 DELIMITER ;
 
 -- ------------------------------------
 --  RETURN STUDENT NAME BY STUDENT ID
 -- ------------------------------------
-DROP PROCEDURE IF EXISTS getStudentNameById;
-DELIMITER $$
-CREATE PROCEDURE getStudentNameById(IN student_id_var INT)
-BEGIN
-SELECT student_name FROM student WHERE student_id = student_id_var;
-END $$
-DELIMITER ;
+-- DROP PROCEDURE IF EXISTS getStudentNameById;
+-- DELIMITER $$
+-- CREATE PROCEDURE getStudentNameById(IN student_id_var INT)
+-- BEGIN
+-- SELECT student_name FROM student WHERE student_id = student_id_var;
+-- END $$
+-- DELIMITER ;
+
+-- ------------------------------------
+--  RETURN STUDENT ID BY STUDENT NAME
+-- ------------------------------------
+-- DROP PROCEDURE IF EXISTS getStudentIdByName;
+-- DELIMITER $$
+-- CREATE PROCEDURE getStudentIdByName(IN student_name VARCHAR(255), OUT student_id INT)
+-- BEGIN
+-- SELECT student_id FROM student WHERE student_name = student_name_var;
+-- END $$
+-- DELIMITER ;
 
 -- ------------------------------------
 --         RETURNS ALL PAPER ID
 -- ------------------------------------
-DROP PROCEDURE IF EXISTS getAllPaperId;
+DROP PROCEDURE IF EXISTS getAllPaperNames;
 DELIMITER $$
-CREATE PROCEDURE getAllPaperId()
+CREATE PROCEDURE getAllPaperNames()
 BEGIN
-SELECT paper_id FROM paper;
+SELECT paper_name FROM paper;
 END $$
 DELIMITER ;
 
 -- ------------------------------------
---   RETURNS PAPER IDS BY STUDENT ID
+--   RETURNS PAPER NAMES BY STUDENT NAME
 -- ------------------------------------
-DROP PROCEDURE IF EXISTS getAllPaperIdByStudentId;
+DROP PROCEDURE IF EXISTS getAllPaperNamesByStudentName;
 DELIMITER $$
-CREATE PROCEDURE getAllPaperIdByStudentId(IN student_id_var INT)
+CREATE PROCEDURE getAllPaperNameByStudentName(IN student_name_var INT)
 BEGIN
-SELECT paper_id FROM paper WHERE student_id = student_id_var;
+SELECT paper_name FROM paper WHERE student_name = student_name_var;
 END $$
 DELIMITER ;
 
@@ -59,23 +70,23 @@ DELIMITER ;
 -- ------------------------------------
 --       RETURN PAPER NAME BY ID
 -- ------------------------------------
-DROP PROCEDURE IF EXISTS getPaperNameById;
-DELIMITER $$
-CREATE PROCEDURE getPaperNameById(IN paper_id_var INT)
-BEGIN
-SELECT paper_name FROM paper WHERE paper_id = paper_id_var;
-END $$
-DELIMITER ;
+-- DROP PROCEDURE IF EXISTS getPaperNameById;
+-- DELIMITER $$
+-- CREATE PROCEDURE getPaperNameById(IN paper_id_var INT)
+-- BEGIN
+-- SELECT paper_name FROM paper WHERE paper_id = paper_id_var;
+-- END $$
+-- DELIMITER ;
 
 
 -- ------------------------------------
 --    RETURN NUMBER OF WORDS BY ID
 -- ------------------------------------
-DROP PROCEDURE IF EXISTS getNumberOfWordsById;
+DROP PROCEDURE IF EXISTS getNumberOfWordsByPaperName;
 DELIMITER $$
-CREATE PROCEDURE getNumberOfWordsById(IN paper_id_var INT)
+CREATE PROCEDURE getNumberOfWordsByPaperName(IN paper_name_var INT)
 BEGIN
-SELECT number_of_words FROM paper WHERE paper_id = paper_id_var;
+SELECT number_of_words FROM paper WHERE paper_name = paper_name_var;
 END $$
 DELIMITER ;
 
@@ -83,11 +94,11 @@ DELIMITER ;
 -- ------------------------------------
 -- RETURN NUMBER OF UNIQUE WORDS BY ID
 -- ------------------------------------
-DROP PROCEDURE IF EXISTS getNumberOfUniqueWordsById;
+DROP PROCEDURE IF EXISTS getNumberOfUniqueWordsByPaperName;
 DELIMITER $$
-CREATE PROCEDURE getNumberOfUniqueWordsById(IN paper_id_var INT)
+CREATE PROCEDURE getNumberOfUniqueWordsByPaperName(IN paper_name_var INT)
 BEGIN
-SELECT number_of_unique_words FROM paper WHERE paper_id = paper_id_var;
+SELECT number_of_unique_words FROM paper WHERE paper_name = paper_name_var;
 END $$
 DELIMITER ;
 
@@ -115,14 +126,23 @@ DELIMITER ;
 -- ------------------------------------
 --          CREATE NEW PAPER
 -- ------------------------------------
+-- Add trigger: if student ID does not exist, create student
 DROP PROCEDURE IF EXISTS createNewPaper;
 DELIMITER $$
-CREATE PROCEDURE createNewPaper(IN paper_name_var VARCHAR(255), IN paper_display_name_var VARCHAR(255), IN number_of_words_var INT, IN number_of_unique_words_var INT, IN student_id_var INT)
+CREATE PROCEDURE createNewPaper(IN paper_name_var VARCHAR(255), IN paper_display_name_var VARCHAR(255), IN number_of_words_var INT, IN number_of_unique_words_var INT, IN student_name_var INT)
 BEGIN
-INSERT INTO paper(paper_name, paper_display_name, number_of_words, number_of_unique_words, student_id)
-VALUES (paper_name_var, paper_display_name_var, number_of_words_var, number_of_unique_words_var, student_id_var);
+INSERT INTO paper(paper_name, paper_display_name, number_of_words, number_of_unique_words, student_name)
+VALUES (paper_name_var, paper_display_name_var, number_of_words_var, number_of_unique_words_var, student_name_var);
 END $$
 DELIMITER ;
+
+-- ------------------------------------
+--             TRIGGERS
+-- ------------------------------------
+
+-- ------------------------------------
+--  CHECK IF STUDENT_ID EXISTS TRIGGER
+-- ------------------------------------
 
 
 -- ------------------------------------
