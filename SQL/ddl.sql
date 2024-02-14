138838b9072d4295ba60a48e7eca2f9b6d58f8ba
Jogvan Patursson <jogvan.pat18@student.setur.fo>
2024-02-14 16:12:42 +0000

    name -> id

--- SQL/ddl.sql
+++ SQL/ddl.sql
@@ -4,19 +4,21 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS student;
 SET FOREIGN_KEY_CHECKS = 1;
 CREATE TABLE student(
+    student_id INT NOT NULL AUTO_INCREMENT,
     student_name VARCHAR(255) NOT NULL UNIQUE,
-    PRIMARY KEY (student_name)
+    PRIMARY KEY (student_id)
 );
 
 SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS paper;
 SET FOREIGN_KEY_CHECKS = 1;
 CREATE TABLE paper(
+    paper_id INT NOT NULL AUTO_INCREMENT,
     paper_name VARCHAR(255) NOT NULL UNIQUE,
     paper_display_name VARCHAR(255) NOT NULL,
     number_of_words int NOT NULL,
     number_of_unique_words int NOT NULL,
-    student_name VARCHAR(255) NOT NULL UNIQUE,
-    PRIMARY KEY (paper_name),
-    FOREIGN KEY(student_name) REFERENCES student(student_name)
+    student_id INT,
+    PRIMARY KEY (paper_id),
+    FOREIGN KEY(student_id) REFERENCES student(student_id)
 );
