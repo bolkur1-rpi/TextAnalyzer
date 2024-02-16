90c525f0254c20fc1d3f62baaf6835b8dffd4528
Slow4life <O-helmdal@hotmail.com>
2024-02-16 11:25:52 +0000

    User info update

--- html/index.php
+++ html/index.php
@@ -9,6 +9,7 @@
 <?php
 session_start();
 if(!isset($_SESSION['login'])) { header("Location: /php/login.php"); }
+$test = "Hetta er content";
 ?>
 
 <script>
@@ -80,7 +81,6 @@ if(!isset($_SESSION['login'])) { header("Location: /php/login.php"); }
   $sql = "SELECT * FROM paper, student WHERE student.student_id = paper.student_id AND student.student_name = '".$_SESSION['login']."'";
   $result = $conn->query($sql);
   //$_SESSION['sub_count'] = $result->num_rows;
-  $test = "Hetta er content";
   if ($result->num_rows > 0) {
       // Output submissions
       while($row = $result->fetch_assoc()) {
