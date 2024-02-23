792ea5b5cab6a192197b867fb66ca0d31613379f
Slow4life <O-helmdal@hotmail.com>
2024-02-23 12:06:11 +0000

    Docker image build fixes

--- docker-SQL/b.sql
+++ docker-SQL/b.sql
@@ -1,111 +1,119 @@
 -- ------------------------------------
 -- FILE FOR FUNCTIONS/STORED PROCEDURES
 -- ------------------------------------
+
 -- ------------------------------------
 --    SPECIFY WHICH DATABASE TO USE
 -- ------------------------------------
 USE textdb;
+
 -- ------------------------------------
 --          SELECT FUNCTIONS
 -- ------------------------------------
+
 -- ------------------------------------
 --       SELECT ALL STUDENT IDS
 -- ------------------------------------
 DROP PROCEDURE IF EXISTS getAllStudentNames;
-delimiter |;
+DELIMITER $$
 CREATE PROCEDURE getAllStudentNames()
 BEGIN
 SELECT student_name FROM student;
-END ;
-|;
-delimiter ;
+END $$
+DELIMITER ;
+
 -- ------------------------------------
 --  SELECT STUDENT NAME BY STUDENT ID
 -- ------------------------------------
 DROP PROCEDURE IF EXISTS getStudentNameById;
-delimiter |;
+DELIMITER $$
 CREATE PROCEDURE getStudentNameById(IN student_id_var INT)
 BEGIN
 SELECT student_name FROM student WHERE student_id = student_id_var;
-END ;
-|;
-delimiter ;
+END $$
+DELIMITER ;
+
 -- ------------------------------------
 --         SELECT ALL PAPER ID
 -- ------------------------------------
 DROP PROCEDURE IF EXISTS getAllPaperNames;
-delimiter |;
+DELIMITER $$
 CREATE PROCEDURE getAllPaperNames()
 BEGIN
 SELECT paper_name FROM paper;
-END ;
-|;
-delimiter ;
+END $$
+DELIMITER ;
+
 -- ------------------------------------
 --   SELECT PAPER NAMES BY STUDENT NAME
 -- ------------------------------------
 DROP PROCEDURE IF EXISTS getAllPaperNamesByStudentName;
-delimiter |;
+DELIMITER $$
 CREATE PROCEDURE getAllPaperNamesByStudentName(IN student_name_var INT)
 BEGIN
 SELECT paper_name FROM paper WHERE student_name = student_name_var;
-END ;
-|;
-delimiter ;
+END $$
+DELIMITER ;
+
+
 -- ------------------------------------
 --       SELECT PAPER NAME BY ID
 -- ------------------------------------
 DROP PROCEDURE IF EXISTS getPaperNameById;
-delimiter |;
+DELIMITER $$
 CREATE PROCEDURE getPaperNameById(IN paper_id_var INT)
 BEGIN
 SELECT paper_name FROM paper WHERE paper_id = paper_id_var;
-END ;
-|;
-delimiter ;
+END $$
+DELIMITER ;
+
+
 -- ------------------------------------
 --    RETURN NUMBER OF WORDS BY ID
 -- ------------------------------------
 DROP PROCEDURE IF EXISTS getNumberOfWordsByPaperName;
-delimiter |;
+DELIMITER $$
 CREATE PROCEDURE getNumberOfWordsByPaperName(IN paper_name_var INT)
 BEGIN
 SELECT number_of_words FROM paper WHERE paper_name = paper_name_var;
-END ;
-|;
-delimiter ;
+END $$
+DELIMITER ;
+
+
 -- ------------------------------------
 -- RETURN NUMBER OF UNIQUE WORDS BY ID
 -- ------------------------------------
 DROP PROCEDURE IF EXISTS getNumberOfUniqueWordsByPaperName;
-delimiter |;
+DELIMITER $$
 CREATE PROCEDURE getNumberOfUniqueWordsByPaperName(IN paper_name_var INT)
 BEGIN
 SELECT number_of_unique_words FROM paper WHERE paper_name = paper_name_var;
-END ;
-|;
-delimiter ;
+END $$
+DELIMITER ;
+
 -- ------------------------------------
 --          INSERT FUNCTIONS
 -- ------------------------------------
+
 -- ------------------------------------
 --         CREATE NEW STUDENT
 -- ------------------------------------
 DROP PROCEDURE IF EXISTS createNewStudent;
-delimiter |;
+DELIMITER $$
 CREATE PROCEDURE createNewStudent(IN student_name_var VARCHAR(255))
 BEGIN
 INSERT INTO student(student_name)
 VALUES (student_name_var);
-END ;
-|;
-delimiter ;
+END $$
+DELIMITER ;
+
 -- ------------------------------------
 --          CREATE NEW PAPER
 -- ------------------------------------
 DROP PROCEDURE IF EXISTS createNewPaper;
-delimiter |;
-CREATE PROCEDURE createNewPaper(IN paper_name_var VARCHAR(255), IN paper_display_name_var VARCHAR(255), IN number_of_words_var INT, IN number_of_unique_words_var INT, IN number_of_sentences_var INT, IN number_of_paragraphs_var INT, IN words_per_sentence_var FLOAT, IN sente>BEGIN
+DELIMITER $$
+CREATE PROCEDURE createNewPaper(IN paper_name_var VARCHAR(255), IN paper_display_name_var VARCHAR(255), IN number_of_words_var INT, IN number_of_unique_words_var INT, IN number_of_sentences_var INT, IN number_of_paragraphs_var INT, IN words_per_sentence_var FLOAT, IN sentences_per_paragraph_var FLOAT, IN student_name_var VARCHAR(255))
+BEGIN
 DECLARE student_id_var INT;
 DECLARE student_exists INT; -- 1 if it exists, 0 if it doesnt exist
 DECLARE last_auto_increment INT;
@@ -127,6 +135,5 @@ IF (SELECT student_exists = 0) THEN
         INSERT INTO paper(paper_name, paper_display_name, number_of_words, number_of_unique_words, number_of_sentences, number_of_paragraphs, words_per_sentence, sentences_per_paragraph, student_id)
         VALUES (paper_name_var, paper_display_name_var, number_of_words_var, number_of_unique_words_var, number_of_sentences_var, number_of_paragraphs_var, words_per_sentence_var, sentences_per_paragraph_var, student_id_var);
 END IF;
-END ;
-|;
-delimiter ;
+END $$
+DELIMITER ;
---
+++ docker-SQL/c.sql
@@ -0,0 +1,2 @@
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'new_password';
