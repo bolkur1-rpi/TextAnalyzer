90c725a2ca8ed747fabf7ef757799064851cf045
Slow4life <O-helmdal@hotmail.com>
2024-02-23 11:10:16 +0000

    Docker image build fixes

--- update.sh
+++ update.sh
@@ -6,4 +6,5 @@ sudo chmod 007 /home/repo/TextAnalyzer/html/temp
 sudo chmod 007 /home/repo/TextAnalyzer/html/uploads
 sudo chmod +x /home/repo/TextAnalyzer/html/bash/copyToUploads.sh
 sudo chmod +x /home/repo/TextAnalyzer/build.sh
+sudo chmod +x /home/repo/TextAnalyzer/run.sh
 sudo rsync -a --delete /home/repo/TextAnalyzer/html/ /var/www/html/
