335666563de398db282f2018eba3914a5ea8b10c
Slow4life <O-helmdal@hotmail.com>
2024-02-24 01:02:05 +0000

    docker finishing touches

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-docker run -dit --name="ta-mysql" -p --restart=always 3306:3306 -e MYSQL_ROOT_PASSWORD=bolkur1 ta-mysql
+docker run -dit --name="ta-mysql" --restart=always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=bolkur1 ta-mysql
 docker run -dit --name="ta-apache" -p 9090:80 ta-apache
 
 docker network rm ta-network
