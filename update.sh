a8b4ed15c9e7d3352806c2ee6871a1f7ad0b7311
Slow4life <O-helmdal@hotmail.com>
2024-02-27 12:05:34 +0000

    changes to update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,8 @@
 #! /usr/bin/bash
-
-cd /home/repo/TextAnalyzer/
 sudo git pull https://github.com/bolkur1-rpi/TextAnalyzer.git
-sudo chmod 007 /home/repo/TextAnalyzer/html/temp
-sudo chmod 007 /home/repo/TextAnalyzer/html/uploads
-sudo chmod +x /home/repo/TextAnalyzer/html/bash/copyToUploads.sh
-sudo chmod +x /home/repo/TextAnalyzer/build.sh
-sudo chmod +x /home/repo/TextAnalyzer/run.sh
-sudo rsync -a --delete /home/repo/TextAnalyzer/html/ /var/www/html/
+sudo chmod 007 ./html/temp
+sudo chmod 007 ./html/uploads
+sudo chmod +x ./html/bash/copyToUploads.sh
+sudo chmod +x ./build.sh
+sudo chmod +x ./run.sh
+sudo rsync -a --delete ./html/ /var/www/html/
