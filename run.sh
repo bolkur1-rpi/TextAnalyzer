ee053a53a1ee67a3d54f47e674c11e5161e1ceb6
Slow4life <O-helmdal@hotmail.com>
2024-02-27 12:25:34 +0000

    changes to update.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,12 @@
 docker run -dit --name="ta-mysql" --restart=always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=bolkur1 ta-mysql
 docker run -dit --name="ta-apache" -p 9090:80 ta-apache
 
-docker network rm ta-network
-docker network create ta-network
+if [ ! "$(docker network ls | grep ta-network)" ]; then
+  echo "Creating network 'ta-network'."
+  docker network create ta-network
+else
+  echo "Network 'ta-network' already exists."
+fi
+
 docker network connect ta-network ta-mysql
 docker network connect ta-network ta-apache
