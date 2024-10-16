af1bae09be84e85cccd8b64d74d408e1b32b6dc5
JogvanPatursson <jogvanpatursson@gmail.com>
2024-10-16 21:04:50 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 sudo git pull https://github.com/bolkur1-rpi/TextAnalyzer.git
-sudo chmod 007 ./html/temp
-sudo chmod 007 ./html/uploads
+sudo chmod 777 ./html/temp
+sudo chmod 777 ./html/uploads
 sudo chmod +x ./html/bash/copyToUploads.sh
 sudo chmod +x ./build.sh
 sudo chmod +x ./run.sh
